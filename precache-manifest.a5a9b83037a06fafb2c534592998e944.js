ce8d4a126bb8222687e0624606b967e04113f2b5
Abhishek Gajendra <abhishekgajendra7@gmail.com>
2022-04-13 00:18:07 +0530

    Updates

---
+++ precache-manifest.a5a9b83037a06fafb2c534592998e944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a25f00d8bcb195c9dc1a",
+    "url": "/textutils/static/css/main.864de417.chunk.css"
+  },
+  {
+    "revision": "a25f00d8bcb195c9dc1a",
+    "url": "/textutils/static/js/main.a25f00d8.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/textutils/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "ea6a5b0a498d3bbe3e2e",
+    "url": "/textutils/static/js/2.ea6a5b0a.chunk.js"
+  },
+  {
+    "revision": "989726db4251d141a7f8",
+    "url": "/textutils/static/js/runtime~main.989726db.js"
+  },
+  {
+    "revision": "4079705e1b4dab859ef9f67f063c216d",
+    "url": "/textutils/index.html"
+  }
+];
